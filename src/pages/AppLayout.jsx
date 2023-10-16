e81b8ad70e7b985d287f98a014b87062e2efb2c4
Danny W <dannywang@utexas.edu>
2023-10-15 21:01:31 -0400

    add fake login function; change website title; bug fix for the background image

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,18 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
+// import { useAuth } from "../contexts/FakeAuthContext";
+import User from "../components/User";
 
 function AppLayout() {
+  // const { isAuthenticated } = useAuth();
+
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      {/* {isAuthenticated ? <User /> : ""} */}
+      <User />
     </div>
   );
 }
